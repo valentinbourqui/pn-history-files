b879e8829177e06840ff8561242c88112f81e1ad
Valentin <valentin@valentins-macbook-pro.home>
2025-02-03 18:23:48 +0100

    update fetch

--- api/fetch-files.js
+++ api/fetch-files.js
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 
 export default async function handler(req, res) {
-  const { query, pageToken } = req.query;
-  const folderId = "1Y0zWn74fRmo32APXO1CMSsZ7b_tfR5z4";
+  const { query, pageToken, language } = req.query;
+  const folderIdFr = "19ZCir_edp2VvHdcOZ1qgQKhYkAAyPVlt";
+  const folderIdDe = "1rRJ_9UG-kEYmuChynpBxJ9RCX5aoa7dj";
   const apiKey = process.env.GOOGLE_API_KEY;
 
   try {
+      const folderId = language == "FR" ? folderIdFr : folderIdDe;
       const response = await fetch(
         `https://www.googleapis.com/drive/v3/files?pageSize=1&q='${folderId}' in parents ${query ? `and (name contains '${query}' or fullText contains '${query}')`:""}&orderBy=name desc&fields=files(id, name),nextPageToken${pageToken ? `&pageToken=${pageToken}` : ""}&key=${apiKey}`
       );
